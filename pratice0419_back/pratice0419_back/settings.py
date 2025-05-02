3befbad757647695888b04fbb75d61afb21e4eb4
陳國翔 <s1411232069@ad1.nutc.edu.tw>
2025-05-02 23:02:24 +0800

    fix(setting):格式

--- pratice0419_back/pratice0419_back/settings.py
+++ pratice0419_back/pratice0419_back/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 CORS_ORIGIN_ALLOW_ALL = True
