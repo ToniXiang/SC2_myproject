01c72558d1d7dc449f387f740d2299cd604f3534
ChenGuoXiang940 <s1411232069@ad1.nutc.edu.tw>
2025-05-23 13:15:04 +0800

    feat(flutter):運行電腦應用程式時鎖定為手機畫面的大小

--- pratice0419/lib/main.dart
+++ pratice0419/lib/main.dart
@@ -2,8 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'login_page.dart';
 import 'thememodenotifier.dart';
-
+import 'package:flutter/foundation.dart';
+import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'dart:io';
+const double windowWidth = 400;
+const double windowHeight = 800;
+void setupWindow() {
+  if (!kIsWeb && (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+    WidgetsFlutterBinding.ensureInitialized();
+    doWhenWindowReady(() {
+      const initialSize = Size(windowWidth, windowHeight);
+      appWindow.minSize = initialSize;
+      appWindow.maxSize = initialSize;
+      appWindow.size = initialSize;
+      appWindow.alignment = Alignment.center;
+      appWindow.title = "資工購物平台";
+      appWindow.show();
+    });
+  }
+}
 void main() {
+  setupWindow();
   runApp(
     ChangeNotifierProvider(
       create: (_) => ThemeModeNotifier(),
