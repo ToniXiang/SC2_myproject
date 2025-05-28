0a3c90f7a3e006def989938763e73ad54a31e81f
ChenGuoXiang940 <s1411232069@ad1.nutc.edu.tw>
2025-05-28 17:12:39 +0800

    feat(django):新增 web 資訊

--- pratice0419_back/pratice0419_back/settings.py
+++ pratice0419_back/pratice0419_back/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY','s04qb=a9hn7y&5uuc0n%)9geze#u5dpqesx^71a9wijtjbqe6_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
